583339d4e5351c0ff3ff0437f7985c52afd53eb3
TerrainLife <embedded.life@yandex.ru>
2020-11-21 18:59:49 +0300

    HW1 work

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	fmt.Println("current time: ", time.Now().Round(time.Second))
+
+	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalf("Can`t get ntp time!")
+	}
+	fmt.Println("exact time: ", ntpTime.Round(time.Second))
+
 }
